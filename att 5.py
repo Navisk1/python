144eb4c7d6a1e43b38e897b5988642c4a1296272
Navisk1 <joao.gabrielccosta9@gmail.com>
2024-05-02 22:09:14 -0300

    criativa

---
+++ att 5.py
@@ -0,0 +1,7 @@
+#Escreva um programa que calcule o fatorial de um número inserido pelo usuário
+#utilizando um laço de repetição while
+fat = 1
+n = int(input("coloque aqui o numero:"))
+while i <= n:
+   fat = fat*i
+   i = i + 1
